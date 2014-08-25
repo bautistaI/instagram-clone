d1be342640531540b6be56c5f9d1c8c5f7be655a
Israel Bautista <bautista.israel@rocketmail.com>
2014-08-24 19:34:59 -0500

    functional app

--- main.js
+++ main.js
@@ -3,26 +3,40 @@ angular.module('myApp', ['ngAnimate'])
     // hide messages by default
     $scope.foundPhotos = true;
     $scope.noPhotos = true;
-    $scope.bigImage = true;
+
+    // hide invalid search message
+    $scope.invalidSearch = true;
     
-    // Whenever we receive our promise back then go ahead and run this
+    // Whenever we receive our promise back from the searchInstagram function, then go ahead and run this
     $scope.submitForm = function(){
-      searchInstagram($scope.tag)
-      .then(function(data){
-        $scope.photos = data.data;
-        $scope.loading = false;
-        $scope.tag = '';
-      }, function(error){
-        $scope.tag = '';
-        $scope.loading = false;
-      });
+      var validForm = $scope.myForm.$valid;
+      var validSearch = $scope.myForm.myTag.$dirty;
+
+      if(validForm && validSearch){
+        $scope.invalidSearch = true;
+        searchInstagram($scope.tag)
+        .then(function(data){
+          $scope.photos = data.data;
+          $scope.loading = false;
+          $scope.tag = '';
+        }, function(error){
+          $scope.tag = '';
+          $scope.loading = false;
+        });
+      }else{
+        $scope.invalidSearch = false;
+      }
     };
 
     //Private function for now, but it could very well be inside of a service. 
     // This would send our promise back to the submitForm whenever is ready. 
     var searchInstagram = function(tag){
+      // instance of defer object
       var defer = $q.defer();
+
+      // hide loading gif animation on true
       $scope.loading = true;
+
       // API end-point
       var url = "https://api.instagram.com/v1/tags/"+ tag +"/media/recent";
        
@@ -31,18 +45,22 @@ angular.module('myApp', ['ngAnimate'])
         client_id: "96c6601d84304b20a374b8064b973535",
         callback: 'JSON_CALLBACK'
       };
-
       // Query parameters
       $http({
         method: 'JSONP',
         url: url,
         params: request
       })
-      .success(function(data){
+      .success(function(data, meta){
         console.log('success', data);
+        // display the error message from the meta object
+        $scope.apiError = data.meta.error_message;
+     
         // show found photos message hide no photos found message
         $scope.foundPhotos = false;
         $scope.noPhotos = true;
+        $scope.searchTag = tag;
+        
         defer.resolve(data);
       })
       .error(function(data){
@@ -53,16 +71,5 @@ angular.module('myApp', ['ngAnimate'])
       });
         // we return the promise whether is success or error
         return defer.promise;
-    };
-
-    // on thumbnail image click hide thumbnail show large image
-    $scope.thumbnailImage = function(){
-      $scope.bigImage = false;
-      $scope.thumbnail = true;
-    };
-    // on large image click hide image show thumbnail
-    $scope.biggerImage = function(){
-      $scope.bigImage = true;
-      $scope.thumbnail = false;
     };
 });
